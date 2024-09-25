773c9f7e3ab0480a7e08cd9d3112edcd63b925e9
ElvisOmbiro <elvisombiro99@gmail.com>
2024-09-25 14:24:23 +0300

    index.js

--- server/routes/index.js
+++
@@ -1,5 +0,0 @@
-import { Express } from 'express';
-import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
-import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-import { APIError, errorResponse } from '../middlewares/error';
